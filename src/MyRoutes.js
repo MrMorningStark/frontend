98bbafc80d33b4231bf22300c15cbcc7da5c9af0
Rajat Khatri <khatrirajat888@gmail.com>
2023-02-10 13:29:09 +0530

    Update MyRoutes.js

--- src/MyRoutes.js
+++ src/MyRoutes.js
@@ -1,16 +1,16 @@
 import { Outlet, useRoutes } from "react-router-dom"
 import { portfolioRoutes } from "./routes/portfolioRoutes"
-import { newsRoutes } from "./routes/newsRoutes";
+import { newsRoutes } from "./routes/newsRoutes";s
 
 export const MyRoutes = () => {
 
     return useRoutes([
         {
             path: '',
+            exact:true,
             element: <Outlet />,
             children: [portfolioRoutes, newsRoutes]
-        },
-
+        }
     ]);
 
 }
