394b8f2035c51544a2f0525a65a19af78e738e01
Rajat Khatri <khatrirajat888@gmail.com>
2023-02-13 14:31:51 +0530

    hashrouter for gitpages
    as gitpages dont support brouser router tech

--- src/MyRoutes.js
+++ src/MyRoutes.js
@@ -1,7 +1,6 @@
-import { useRoutes } from "react-router-dom"
+import { Navigate, useRoutes } from "react-router-dom"
 import { portfolioRoutes } from "./routes/portfolioRoutes"
 import { newsRoutes } from "./routes/newsRoutes";
-import { NotFound404 } from "./modules/notFound404/NotFound404";
 
 
 export const MyRoutes = () => {
@@ -10,8 +9,8 @@ export const MyRoutes = () => {
         portfolioRoutes,
         newsRoutes,
         {
-            path:'',
-            element: <NotFound404 />,
+            path:'/',
+            element: <Navigate to={'/my-portfolio'} />,
         }
     ]);
 
