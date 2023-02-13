413a726aa798887019623ea1132ddb2ec6b43b70
Rajat Khatri <khatrirajat888@gmail.com>
2023-02-13 14:22:58 +0530

    Update Content.js

--- src/modules/helper/Content.js
+++ src/modules/helper/Content.js
@@ -6,7 +6,7 @@ export const Content = ({ url, img_url, title, subTitle, updated_date, img_capti
         <h1 className="news_heading">{title}</h1>
         <p className="news_date">{updated_date}</p>
         <h2 className="news_sub_heading">{subTitle}</h2>
-        <img alt="img" className="news_img" src={img_url} onClick={() => { window.open(img_url) }} />
+        <img alt="img" className="news_img" src={img_url} />
         <p className="news_img_caption">{img_caption}</p>
         {/* <div className="content-right">
             
